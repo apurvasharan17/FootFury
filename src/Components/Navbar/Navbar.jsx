b1385f918b77864e0dbecd489484f998b5a8b30c
Apurva Sharan <ranjan.apurva2017@gmail.com>
2024-07-27 17:42:12 +0530

    logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -22,7 +22,7 @@ export const Navbar = () => {
           <Link to="/" className="ranger-logo">
             <img
               // src="https://res.cloudinary.com/bhakti1801/image/upload/v1649919109/R_ezqx3h.png"
-              src="../../public/assets/R.png"
+              src="./assets/R.png"
               alt="logo"
               className="logo-ecom"
             />
